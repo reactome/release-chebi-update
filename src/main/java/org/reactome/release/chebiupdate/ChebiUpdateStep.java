50ce548cd2f4afbfe5c5b4ac64afe308254e02a0
Derek Tang <d2tang@oicr.on.ca>
2024-04-04 09:58:52 -0400

    use curator from dbutils

--- src/main/java/org/reactome/release/chebiupdate/ChebiUpdateStep.java
+++ src/main/java/org/reactome/release/chebiupdate/ChebiUpdateStep.java
@@ -4,13 +4,14 @@ import java.util.Properties;
 
 import org.gk.persistence.MySQLAdaptor;
 import org.reactome.release.common.ReleaseStep;
+import org.reactome.util.general.DBUtils;
 
 public class ChebiUpdateStep extends ReleaseStep
 {
 	@Override
 	public void executeStep(Properties props) throws Exception
 	{
-		MySQLAdaptor adaptor = getMySQLAdaptorFromProperties(props);
+		MySQLAdaptor adaptor = DBUtils.getCuratorDbAdaptor(props);
 		this.loadTestModeFromProperties(props);
 		long personID = new Long(props.getProperty("personId"));
 		boolean useCache = Boolean.parseBoolean(props.getProperty("useCache", "false"));
