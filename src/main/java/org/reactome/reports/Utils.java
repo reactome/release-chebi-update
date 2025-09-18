dddb2b11a3f95abcce20f2dc7f794564ea416fe7
Joel Weiser <joel.weiser@oicr.on.ca>
2025-09-17 20:07:17 -0400

    removes logger statement

--- src/main/java/org/reactome/reports/Utils.java
+++ src/main/java/org/reactome/reports/Utils.java
@@ -1,20 +1,13 @@
 package org.reactome.reports;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.gk.model.GKInstance;
 import org.gk.model.ReactomeJavaConstants;
-import org.gk.schema.InvalidAttributeException;
 
 public class Utils {
-    private static Logger logger = LogManager.getLogger();
 
-    public static GKInstance getCreator(GKInstance inst) throws InvalidAttributeException, Exception {
+    public static GKInstance getCreator(GKInstance inst) throws Exception {
         GKInstance createdInstanceEdit = (GKInstance) inst.getAttributeValue(ReactomeJavaConstants.created);
         if (createdInstanceEdit == null) {
-            // User should probably be warned that the object has no "creator" attribute value
-            // so they can explain to the curators why there is no author name in the report.
-            logger.warn("Instance {} does not have a value for \"created\" attribute!", inst.toString());
             return null;
         }
         GKInstance creator = (GKInstance) createdInstanceEdit.getAttributeValue(ReactomeJavaConstants.author);
